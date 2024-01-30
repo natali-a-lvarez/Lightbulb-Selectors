39c7cadeb9771262bbd35cb8facf3710433fa734
natali_a_lvarez <03nataliaalvarez07@gmail.com>
2024-01-30 16:31:03 -0500

    adding bulb logic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Write your code here
+let lightbulbs = document.querySelectorAll(".item");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+console.log(lightbulbs);
+
+lightbulbs.forEach((el) => {
+  console.log(el);
+
+  el.addEventListener("click", function () {
+    count++;
+    el.classList.toggle("active");
+    subtitle.innerHTML = `You've clicked the lights ${count} ${
+      count > 1 ? "times" : "time"
+    }`;
+  });
+});
